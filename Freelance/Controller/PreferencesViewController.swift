8d0276204f8b1910214946cfdcb493a508e9c1ea
ulascc <ulas.can2424@gmail.com>
2024-01-24 17:43:31 +0300

    dark mode switch added

--- Freelance/Controller/PreferencesViewController.swift
+++ Freelance/Controller/PreferencesViewController.swift
@@ -8,9 +8,59 @@
 import UIKit
 
 class PreferencesViewController: UIViewController {
-
+    
+    @IBOutlet weak var notificationsSwitch: UISwitch!
+    @IBOutlet weak var microphoneAndCameraSwitch: UISwitch!
+    @IBOutlet weak var locationSwitch: UISwitch!
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("this is a preferences screen")
+        print("This is a preferences screen")
+        
+        // Kullanıcının tercihine göre temayı ayarla
+        updateTheme()
+    }
+    
+    // Switch değeri değiştiğinde bu fonksiyon çağrılır
+    @IBAction func darkThemeSwitchValueChanged(_ sender: UISwitch) {
+        // Kullanıcının tercihini sakla
+        UserDefaults.standard.set(sender.isOn, forKey: "DarkThemeEnabled")
+        
+        // Tüm uygulamadaki temayı güncelle
+        updateGlobalTheme()
+    }
+    
+    func updateTheme() {
+        // Switch değerine göre koyu tema veya varsayılan tema seçimi
+        let darkThemeEnabled = UserDefaults.standard.bool(forKey: "DarkThemeEnabled")
+        darkModeSwitch.isOn = darkThemeEnabled
+        updateGlobalTheme()
+    }
+    
+    func updateGlobalTheme() {
+        // UserDefaults'tan kullanıcının tercihini al
+        let darkThemeEnabled = UserDefaults.standard.bool(forKey: "DarkThemeEnabled")
+        
+        // Tüm uygulamadaki temayı güncelle
+        if darkThemeEnabled {
+            // Koyu tema
+            UIApplication.shared.connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .forEach { windowScene in
+                    windowScene.windows.forEach { window in
+                        window.overrideUserInterfaceStyle = .dark
+                    }
+                }
+        } else {
+            // Varsayılan tema
+            UIApplication.shared.connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .forEach { windowScene in
+                    windowScene.windows.forEach { window in
+                        window.overrideUserInterfaceStyle = .light
+                    }
+                }
+        }
     }
 }

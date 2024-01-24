be9a693bbe8ab527aa590a1042b2476e5403d5a2
ulascc <ulas.can2424@gmail.com>
2024-01-24 04:30:21 +0300

    profile update added

---
+++ Freelance/Controller/EditProfileViewController.swift
@@ -0,0 +1,90 @@
+import UIKit
+import Firebase
+import FirebaseFirestore
+
+class EditProfileViewController: UIViewController {
+
+    var fullName: String?
+    var phoneNumber: String?
+    var city: String?
+    var about: String?
+    
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var aboutTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is an edit profile screen")
+        
+        fullNameTextField.text = fullName
+        phoneNumberTextField.text = phoneNumber
+        cityTextField.text = city
+        aboutTextField.text = about
+    }
+    
+    
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+        // Oturum açmış kullanıcının e-posta adresini al
+        if let userEmail = Auth.auth().currentUser?.email {
+            let db = Firestore.firestore()
+            
+            // Kullanıcıların bulunduğu "users" koleksiyonundaki dokümanı güncelle
+            let userRef = db.collection("users").whereField("email", isEqualTo: userEmail)
+            
+            userRef.getDocuments { (querySnapshot, error) in
+                if let error = error {
+                    print("Error getting documents: \(error.localizedDescription)")
+                } else {
+                    // Her kullanıcı için döngü
+                    for document in querySnapshot!.documents {
+                        let documentID = document.documentID
+                        
+                        // TextField'ların değerleriyle Firestore dokümanını güncelle
+                        let updatedData: [String: Any] = [
+                            "about": self.aboutTextField.text ?? "",
+                            "city": self.cityTextField.text ?? "",
+                            "phoneNumber": self.phoneNumberTextField.text ?? "",
+                            "fullName": self.fullNameTextField.text ?? ""
+                        ]
+                        
+                        // Dokümanı güncelle
+                        db.collection("users").document(documentID).updateData(updatedData) { error in
+                            if let error = error {
+                                print("Error updating document: \(error.localizedDescription)")
+                                // Güncelleme başarısız ise hata bildirimi göster
+                                self.showErrorAlert()
+                            } else {
+                                print("Document updated successfully")
+                                
+                                // Güncelleme başarılı ise bildirim göster ve ProfileViewController'a yönlendir
+                                self.showSuccessAlert()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func showSuccessAlert() {
+        let alertController = UIAlertController(title: "Successfully", message: "The profile has been updated successfully.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            // ProfileViewController ekranına yönlendir ve ekranı reload et
+            self.navigationController?.popViewController(animated: true)
+            if let profileVC = self.navigationController?.viewControllers.last as? ProfileViewController {
+                profileVC.fetchUserProfile()
+            }
+        }
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func showErrorAlert() {
+        let alertController = UIAlertController(title: "Error", message: "An error occurred while updating the profile.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
--- Freelance/Controller/ProfileViewController.swift
+++ Freelance/Controller/ProfileViewController.swift
@@ -4,39 +4,44 @@ import FirebaseFirestore
 import FirebaseStorage
 
 class ProfileViewController: UIViewController {
-
+    
     @IBOutlet weak var fullnameLabel: UILabel!
     @IBOutlet weak var aboutLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var phoneNumberLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
-
+    
     let db = Firestore.firestore()
     let storage = Storage.storage()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchUserProfile()
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        fetchUserProfile()
+    }
+    
     func fetchUserProfile() {
         // Kullanıcının UID'sini al
         if let user = Auth.auth().currentUser {
             let userRef = db.collection("users").document(user.uid)
-
+            
             // Firestore'daki kullanıcı verilerini çek
             userRef.getDocument { (document, error) in
                 if let document = document, document.exists {
                     let userData = document.data()!
-
+                    
                     // Firestore'dan gelen veriyi kullanarak kullanıcının profil bilgilerini güncelle
                     self.fullnameLabel.text = userData["fullName"] as? String
                     self.aboutLabel.text = userData["about"] as? String
                     self.emailLabel.text = userData["email"] as? String
                     self.phoneNumberLabel.text = userData["phoneNumber"] as? String
                     self.cityLabel.text = userData["city"] as? String
-
+                    
                     // Profil fotoğrafını göstermek için Storage'dan URL al ve yükle
                     if let profileImageUrl = userData["profileImageUrl"] as? String {
                         self.loadProfileImage(with: profileImageUrl)
@@ -47,10 +52,10 @@ class ProfileViewController: UIViewController {
             }
         }
     }
-
+    
     func loadProfileImage(with imageUrl: String) {
         let storageRef = storage.reference(forURL: imageUrl)
-
+        
         // Storage'dan profil fotoğrafını indir
         storageRef.getData(maxSize: 1 * 1024 * 1024) { (data, error) in
             if let error = error {
@@ -60,6 +65,23 @@ class ProfileViewController: UIViewController {
                 if let imageData = data, let profileImage = UIImage(data: imageData) {
                     self.profileImageView.image = profileImage
                 }
+            }
+        }
+    }
+    
+    
+    @IBAction func EditProfileButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "EditProfileSegue", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditProfileSegue" {
+            if let destinationVC = segue.destination as? EditProfileViewController {
+                // Verileri güvenli bir şekilde atamak için nil kontrolü yapın
+                destinationVC.fullName = fullnameLabel.text ?? ""
+                destinationVC.about = aboutLabel.text ?? ""
+                destinationVC.phoneNumber = phoneNumberLabel.text ?? ""
+                destinationVC.city = cityLabel.text ?? ""
             }
         }
     }
